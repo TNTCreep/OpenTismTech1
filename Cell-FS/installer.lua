f1b8eaef6e4939338c23da681e14cc77e69474b7
TNTCreep <tntcreep@yahoo.com>
2025-05-23 12:10:00 +0100

    Add files via upload

---
+++ Cell-FS/installer.lua
@@ -0,0 +1,11 @@
+local shell = require("shell")
+local term = require("term")
+term.write("\nInstall Path: ")
+local installPath = string.gsub(term.read(), "\n", "") .. "/"
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/cd_cell.lua " .. installPath .. "cd_cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/cell.lua " .. installPath .. "cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/delete_cell.lua " .. installPath .. "delete_cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/programs.cfg " .. installPath .. "programs.cfg")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/copy_cell.lua " .. installPath .. "copy_cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/rename_cell.lua " .. installPath .. "rename_cell.lua")
+print("Done!")
