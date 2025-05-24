2f9f4993d26ddb1a111744ad50f59559e81b18b9
TNT_Creep <tntcreep@yahoo.com>
2025-05-24 23:30:54 +0100

    Update installergraphite.lua

--- Cell-FS/installergraphite.lua
+++ Cell-FS/installergraphite.lua
@@ -3,7 +3,7 @@ local term = require("term")
 term.write("\nInstall Path: ")
 local installPath = string.gsub(term.read(), "\n", "") .. "/"
 shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/cd_cell.lua " .. installPath .. "cd_cell.lua")
-shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/Cell-FS-Graphite.lua " .. installPath .. "cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/colouredmod/cell-fs-graphite.lua " .. installPath .. "cell.lua")
 shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/delete_cell.lua " .. installPath .. "delete_cell.lua")
 shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/programs.cfg " .. installPath .. "programs.cfg")
 shell.execute("wget https://raw.githubusercontent.com/TNTCreep/OpenTismTech1/refs/heads/main/Cell-FS/copy_cell.lua " .. installPath .. "copy_cell.lua")
